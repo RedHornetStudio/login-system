86da3fc08d04a72576c78ac4d33ad449569ade9d
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2019-12-01 15:50:26 +0200

    new file:   .gitignore new file:   css/style.css new file:   img/RedHornetLogo.png new file:   index.php new file:   templates/footer.php new file:   templates/header.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    require('templates/header.php');
+?>
+
+    <main>
+        <p>aaaaaaaaa</p>
+    </main>
+
+<?php
+    require('templates/footer.php');
+?>
---
+++ templates/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="This is an example of a meta descritpion. This will often show up in search results.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="#">
+                <img src="img/RedHornetLogo.png" alt="Red Hornet Logo" height="100px">
+            </a>
+        </nav>
+    </header>
